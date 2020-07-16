fe6632eaeabc5e68321a46541e830f1efb39a429
brett ohland <brett@ohland.ca>
2020-07-16 07:30:08 -0600

    Removing the safety from the deploy script, let's do this

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ USER=ff6600
 HOST=lunenburg.dreamhost.com             
 DIR=ampersandsoftworks.com
 
-hugo && rsync -avzn --delete public/ ${USER}@${HOST}:~/${DIR}
+hugo && rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR}
 
 exit 0
